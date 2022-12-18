38363786d26f8a292091651d75b9f3bb9393665e
Robert Ryan <robert.ryan@mindspring.com>
2022-12-18 13:42:39 -0800

    Initial Commit

---
+++ SerialTaskDemo/SerialTaskDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerialTaskDemoApp.swift
+//  SerialTaskDemo
+//
+//  Created by Robert Ryan on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SerialTaskDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
